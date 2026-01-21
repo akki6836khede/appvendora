efb449a6b295efc1e71e9f567fca6f93e15652ff
Atharv Khede <atharvkhede2004@gmail.com>
2026-01-21 23:48:34 +0530

    Update route.jsx

--- app/api/auth/[...nextauth]/route.jsx
+++ app/api/auth/[...nextauth]/route.jsx
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
-import checkUserExistence from "@/actions/checkUserExistence"
 
 export const authOptions = {
   providers: [
@@ -9,20 +8,7 @@ export const authOptions = {
       clientSecret: process.env.GOOGLE_SECRET,
     }),
   ],
-
   secret: process.env.NEXTAUTH_SECRET,
-
-callbacks: {
-  async session({ session }) {
-    console.log("SESSION CALLBACK RUNNING", session.user.email)
-
-    const user = await checkUserExistence(session.user.email)
-
-    console.log("USER FROM DB:", user)
-
-    session.user.role = user?.role ?? null
-    return session
-  },
 }
 
 const handler = NextAuth(authOptions)

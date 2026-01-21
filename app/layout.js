bb9871d75114d69372d6b592596cb20e019fa83d
Atharv Khede <atharvkhede2004@gmail.com>
2026-01-21 19:41:37 +0530

    initial2commit

--- app/layout.js
+++ app/layout.js
@@ -25,10 +25,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link
-          rel="stylesheet"
-          href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined"
-        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
